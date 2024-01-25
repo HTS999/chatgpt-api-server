44fcc438571f074bf9ff50ec9e40dc51aec2f8da
Dooy <dooy520@qq.com>
2024-01-25 15:03:35 +0800

    readme

--- release.sh
+++ release.sh
@@ -3,12 +3,12 @@
 set -e
 
 gf build main.go -a amd64 -s linux
-gf docker main.go -t xyhelper/chatgpt-api-server:latest
+gf docker main.go -t hts999/chatgpt-api-server:latest
 # 修改镜像标签为当前日期时间
 time=$(date "+%Y%m%d%H%M%S")
 # 获取当前git commit id 
 commit_id=$(git rev-parse HEAD)
-docker tag xyhelper/chatgpt-api-server:latest xyhelper/chatgpt-api-server:$time-$commit_id
+docker tag hts999/chatgpt-api-server:latest hts999/chatgpt-api-server:$time-$commit_id
 # 推送镜像到docker hub
-docker push xyhelper/chatgpt-api-server:latest
-docker push xyhelper/chatgpt-api-server:$time-$commit_id
+docker push hts999/chatgpt-api-server:latest
+docker push hts999/chatgpt-api-server:$time-$commit_id
